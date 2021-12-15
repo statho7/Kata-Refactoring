47133d993e99e5ae1910a034e1164f05dd19b8fe
statho7 <andreas.stathop@gmail.com>
2021-12-15 19:20:35 +0000

    glue

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    abstract public class MyItem
+    {
+        public abstract void CheckItem(Item item);
+
+        public void DecreaseQuality(Item item, int value = 1)
+        {
+            item.Quality = item.Quality - value;
+        }
+
+        public void IncreaseQuality(Item item, int value = 1)
+        {
+            item.Quality = item.Quality + value;
+        }
+    }
+
+    public class Brie : MyItem
+    {
+        Item _item;
+
+        public Brie(Item item) { _item = item; }
+
+        public override void CheckItem(Item item) {
+
+            item.SellIn = item.SellIn - 1;
+            if (item.SellIn < 0)
+            {
+                if (item.Quality < 50)
+                {
+                    IncreaseQuality(item);
+                }
+            }
+            if (item.Quality < 50)
+            {
+                IncreaseQuality(item);
+            }
+        }
+    }
+
+    public class Concert : MyItem
+    {
+        Item _item;
+        public Concert(Item item) { _item = item; }
+
+        public override void CheckItem(Item item) {
+            item.SellIn = item.SellIn - 1;
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    if (item.SellIn < 6)
+                    {
+                        IncreaseQuality(item, 3);
+                    }
+                    else if (item.SellIn < 11)
+                    {
+                        IncreaseQuality(item, 2);
+                    }
+                    else
+                    {
+                        IncreaseQuality(item);
+                    }
+                }
+            }
+        }
+    }
+
+    public class Other : MyItem
+    {
+        Item _item;
+        public Other(Item item) { _item = item; }
+
+        public override void CheckItem(Item item) {
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
+            {
+                if (item.Quality > 0)
+                {
+                    DecreaseQuality(item);
+                }
+            }
+            if (item.Quality > 0)
+            {
+                DecreaseQuality(item);
+            }
+        }
+    }
+
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void DecreaseQuality(Item item, int value = 1)
+        {
+            item.Quality = item.Quality - value;
+        }
+
+        public void IncreaseQuality(Item item, int value = 1)
+        {
+            item.Quality = item.Quality + value;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                switch (item.Name)
+                {
+                    case "Aged Brie":
+                        Brie brie = new Brie(item);
+                        brie.CheckItem(item);
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        Concert concert = new Concert(item);
+                        concert.CheckItem(item);
+                        break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        break;
+                    default:
+                        Other other = new Other(item);
+                        other.CheckItem(item);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,119 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+using System;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+				// this conjured item does not work properly yet
+				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            GildedRose app = new GildedRose(Items);
+
+            string output = "";
+            string expected = "+5 Dexterity Vest, -20, 0\n"+
+                "Aged Brie, -28, 50\n" +
+                "Elixir of the Mongoose, -25, 0\n" +
+                "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+                "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+                "Backstage passes to a TAFKAL80ETC concert, -15, 0\n" +
+                "Backstage passes to a TAFKAL80ETC concert, -20, 0\n" +
+                "Backstage passes to a TAFKAL80ETC concert, -25, 0\n" +
+                "Conjured Mana Cake, -27, 0\n";
+                
+
+            for (var i = 0; i < 31; i++)
+            {
+                //Console.WriteLine("-------- day " + i + " --------");
+                //Console.WriteLine("name, sellIn, quality");
+                if (i == 30)
+                {
+                    for (var j = 0; j < Items.Count; j++)
+                    {
+                        output += Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality + "\n";
+                    }
+                }
+                app.UpdateQuality();
+            }
+
+            //app.UpdateQuality();
+            Assert.Equal(expected, output);
+        }
+    }
+}
+
+
+//IList<Item> Items = new List<Item>{
+//                new Item ,
+//                new Item ,
+//                new Item ,
+//                new Item ,
+//                new Item ,
+//                new Item
+//                ,
+//                new Item
+//                ,
+//                new Item
+//                {
+//                    Name = "Backstage passes to a TAFKAL80ETC concert",
+//                    SellIn = 5,
+//                    Quality = 49
+//                },
+//				// this conjured item does not work properly yet
+//				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+//            };
+
+//var app = new GildedRose(Items);
+
+
+//for (var i = 0; i < 31; i++)
+//{
+//    Console.WriteLine("-------- day " + i + " --------");
+//    Console.WriteLine("name, sellIn, quality");
+//    for (var j = 0; j < Items.Count; j++)
+//    {
+//        System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+//    }
+//    Console.WriteLine("");
+//    app.UpdateQuality();
+//}
+
+//+5 Dexterity Vest, -20, 0
+//Aged Brie, -28, 50
+//Elixir of the Mongoose, -25, 0
+//Sulfuras, Hand of Ragnaros, 0, 80
+//Sulfuras, Hand of Ragnaros, -1, 80
+//Backstage passes to a TAFKAL80ETC concert, -15, 3
+//Backstage passes to a TAFKAL80ETC concert, -20, 3
+//Backstage passes to a TAFKAL80ETC concert, -25, 3
+//Conjured Mana Cake, -27, 0
